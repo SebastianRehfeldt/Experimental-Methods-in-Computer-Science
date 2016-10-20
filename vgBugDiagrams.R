ea42918855046974b5bde29ada03ee9374872f7a
Sebastian Rehfeldt <Sebastian.Rehfeldt@gmx.net>
2016-10-20 23:17:24 +0100

    pruned boxplot

--- vgBugDiagrams.R
+++ vgBugDiagrams.R
@@ -1,3 +1,8 @@
+#to get total data remove loc from subset and increase ylim for boxplot to 500
+
+withoutBugs = subset(processedData,defects==0 & loc <= 37)
+withBugs = subset(processedData,defects==1 & loc <= 37)
+
 p1 <- hist(withoutBugs[,2],breaks = 100)
 p2 <- hist(withBugs[,2][withBugs[,2]<150],breaks = 100) #remove some lines to make picture clearer
 
@@ -8,5 +13,5 @@ legend("topright", c("Without Bugs", "With Bugs"), col=c(rgb(0,0,1,1/4), col=rgb
 box()
 
 par(mfrow=c(1,2))
-boxplot(withoutBugs[,2],log = "y", main="Modules without bugs", ylab="McCabe Score",pars=list(ylim=c(1,500)))
-boxplot(withBugs[,2],log = "y", main="Modules with bugs", ylab="McCabe Score",pars=list(ylim=c(1,500)))
+boxplot(withoutBugs[,2],log = "y", main="Modules without bugs", ylab="McCabe Score",pars=list(ylim=c(1,50)))
+boxplot(withBugs[,2],log = "y", main="Modules with bugs", ylab="McCabe Score",pars=list(ylim=c(1,50)))
