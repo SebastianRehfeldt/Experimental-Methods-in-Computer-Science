97f8daf394df19b81737188fff3aa0d81ba7faf9
Sebastian Rehfeldt <Sebastian.Rehfeldt@gmx.net>
2016-10-21 11:05:46 +0100

    changed pruning for mc cabe

--- vgBugDiagrams.R
+++ vgBugDiagrams.R
@@ -1,7 +1,7 @@
-#to get total data remove loc from subset and increase ylim for boxplot to 500
+#to get total data remove lOCode from subset and increase ylim for boxplot to 500
 
-withoutBugs = subset(processedData,defects==0 & loc <= 37)
-withBugs = subset(processedData,defects==1 & loc <= 37)
+withoutBugs = subset(processedData,defects==0 & lOCode <= 24)
+withBugs = subset(processedData,defects==1 & lOCode <= 24)
 
 p1 <- hist(withoutBugs[,2],breaks = 100)
 p2 <- hist(withBugs[,2][withBugs[,2]<150],breaks = 100) #remove some lines to make picture clearer
@@ -13,5 +13,5 @@ legend("topright", c("Without Bugs", "With Bugs"), col=c(rgb(0,0,1,1/4), col=rgb
 box()
 
 par(mfrow=c(1,2))
-boxplot(withoutBugs[,2],log = "y", main="Modules without bugs", ylab="McCabe Score",pars=list(ylim=c(1,50)))
-boxplot(withBugs[,2],log = "y", main="Modules with bugs", ylab="McCabe Score",pars=list(ylim=c(1,50)))
+boxplot(withoutBugs[,2],log = "y", main="Modules without bugs", ylab="McCabe Score",pars=list(ylim=c(1,200)))
+boxplot(withBugs[,2],log = "y", main="Modules with bugs", ylab="McCabe Score",pars=list(ylim=c(1,200)))
