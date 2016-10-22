340cf086f766c6c2b9e457c5e63bd83ae2511929
Jani Hilliaho <jani.hilliaho@gmail.com>
2016-10-22 15:58:04 +0100

    backup LogReg

--- AIC-plot.R
+++ AIC-plot.R
@@ -1,15 +1,26 @@
+# This script needs the result of LogReg.R to work correctly
+# You can also use
+
 require(plotrix)
 
-data=c(0, 7139, 7401, 7414, 7421, 7423, 7579, 7622, 7636, 7651, 7660, 7664, 7678, 7701, 7729, 7743, 7743, 7749, 7757, 7758, 7803, 7816)
-titles =c("", "loc", "iv(g)", "v(g)",  "branchCount", "lOBlank", "b", "lOCode", "lOComment", "t", "uniq_Opnd", "total_Opnd", "ev(g)", "total_Op", "n", "d", "LocCoAndCom.","e", "l", "v", "i", "uniq_Op")
+data = c(0);
+titles = c("");
+for (i in 1:length(res[,2])) {
+  data[i+1] = as.numeric(res[i,2])
+  titles[i+1] = res[i,1]
+}
 
 # Set margins
-op <- par(mar = c(8,2,2,1) + 0.1) ## default is c(5,4,4,2) + 0.1
+par(mar = c(8,2,2,1) + 0.1)
 
-gap.barplot(data,gap=c(100,lowest * 0.98),xlab="",ytics=c(0,7000, 7200, 7400, 7600, 7800, 8000),
-            ylab="",main="AIC values with differet metrices", col=c("white"),
-            xlim=c(1.5,length(twogrp)+0.5), ylim=c(0,1000), xaxlab = c(-1,1:(length(twogrp)-1)), xaxs = "i", xaxt="n", yaxs = "i"
-            )
+# Draw barplot
+gap.barplot(data,
+            gap=c(100,8400),  # Break Y-axis
+            ytics=c(0,8400, 8600, 8800, 9000), # Set Y-axis ticks
+            ylab="",main="AIC values with different metrices", col=c("white"), xlab="",
+            xlim=c(1.5,length(twogrp)+0.5), ylim=c(0,800), xaxlab = c(-1,1:(length(twogrp)-1)), xaxs = "i", xaxt="n", yaxs = "i"
+)
+
+# Set X-axis labels
 axis(1, labels=titles, at = seq(1, 22, by = 1), las=2, cex.axis=1)
 
-par(op)
--- LogReg.R
+++ LogReg.R
@@ -10,12 +10,12 @@ library(xtable)
 allColumns <- c("loc", "v(g)", "ev(g)", "iv(g)", "n", "v", "l", "d", "i", "e", "b", "t", "lOCode", "lOComment", "lOBlank", "LocCodeAndComment", "uniq_Op", "uniq_Opnd", "total_Op", "total_Opnd", "branchCount")
 
 # Create a data frame for results
-s_results <- matrix(0, ncol = 3, nrow = 0)
+s_results <- matrix(0, ncol = 2, nrow = 0)
 s_results <- data.frame(s_results)
-colnames(s_results) = c("METRIC", "AUC", "AIC")
+colnames(s_results) = c("METRIC", "AIC")
 
 # Shuffle the data
-shuffled <- processedData[sample(nrow(processedData)),]
+shuffled <- halsteadData[sample(nrow(halsteadData)),]
 
 count = 0;
 # Calculate logistic regression for every single metric against defects
@@ -24,31 +24,15 @@ for (str in allColumns) {
   data = shuffled[c(str, "defects")]
   
   # Use logarithms of the original values
-  for (i in 1:length(data[,1])) {
+  for (i in 1:2) {
     if (data[i,1] != 0) {
       data[i,1] = log(data[i,1])
     }
   }
   
-  train <- data[1:8000,]
-  test <- data[8001:10880,]
-  
-  model <- glm(defects ~.,family=binomial(link='logit'),data=train)
-  anova(model, test="Chisq")
-  pR2(model)
-  
-  p <- predict(model, newdata=subset(test,select=str), type="response")
-  pr <- prediction(p, test$defects)
-  prf <- performance(pr, measure = "tpr", x.measure = "fpr")
-  
-  # Draw the ROC curve
-  # plot(prf)
-
-  auc <- performance(pr, measure = "auc")
-  auc <- auc@y.values[[1]]
+  model <- glm(data$defects ~data[,str],family=binomial(link='logit'))
   aic = model$aic
-
-  s_results[count,] = c(str, round(auc, digits = 3), round(aic, digits = 0))
+  s_results[count,] = c(str, round(aic, digits = 0))
 }
 
 # Sort and print the results
